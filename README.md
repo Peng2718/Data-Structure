1127c846c0a3045c2639242ebf604b456d7cfcde
Peng <1793885447@qq.com>
2026-02-11 10:59:41 +0800

    list:pushback and pushfront

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Data-Structure
+# Data-Structure 数据结构
 This is a Data Structure Learning record during my holiday!
+这是我的寒假数据结构学习记录，欢迎一起学习！
+
+`学习网站`:[算法导航](https://algo.codefather.cn/)
+
+2月11
+我研究了list，发现pushback是将元素加到尾部，而pushfront是从头部开始加，且都是增加元素操作，即不会改变原有元素，而是增加元素
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+
+	list<int> l;
+	l.resize(10);
+	l.push_front(2);
+	for (auto& it : l) {
+		cout << it;
+	}
+	cout << endl;
+	l.push_back(5);
+	for (auto& it : l) {
+		cout << it;
+	}
+	cout << endl;
+	l.push_front(3);
+	for (auto& it : l) {
+		cout << it;
+	}
+
+	return 0;
+}
